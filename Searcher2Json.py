56d4a8aab4e98984ea60f77d53b04ea95bc0bf24
R1chardy <yuanrichard33@gmail.com>
2023-05-11 20:52:02 -0700

    web searcher that given an array of stocks, sites, and a date range, finds all stocks from the site on each day and stores it in a json

---
+++ Searcher2Json.py
@@ -0,0 +1,51 @@
+import siteScrapers.YahooNewsScraper as YahooNewsScraper
+from googlesearch import search
+import json
+from datetime import datetime
+import pandas as pd
+
+def getLinks(stocks, sites, start, end):
+    try:
+        dates = generateDateArray(start, end)
+        pages = set()
+
+        for stock in stocks:
+            for site in sites:
+                for date1, date2 in zip(dates, dates[1:]):
+                    links = googleSearch("site:" + site + " after:" + date1 + " before:" + date2 + " " + stock, 5)
+                    for link in links:
+                        pages.add(link)
+                        print(link)
+        
+        file = open("Searcher2JsonLinks.json", "w")
+        jsonString = json.dumps([link for link in pages])
+        file.write(jsonString)
+        file.close()
+        print("done!")
+    except Exception as e:
+        print(e)
+        file = open("Searcher2JsonLinks.json", "w")
+        jsonString = json.dumps([link for link in pages])
+        file.write(jsonString)
+        file.close()
+        print("done!")
+
+def googleSearch(query, nums):
+    links = search(query, tld="com", num=nums, stop=nums, pause=5)
+    return links
+    
+def generateDateArray(start, end):
+    start_date = datetime.strptime(start, "%Y-%m-%d")
+    end_date = datetime.strptime(end, "%Y-%m-%d")
+    D = 'D'
+    date_list = pd.date_range(start_date, end_date, freq=D)
+    return date_list.strftime("%Y-%m-%d")
+
+
+def main():
+    stocks = ["tesla", "apple", "microsoft", "NVIDIA", "zoom", "blizzard", "google", "iqiyi", "cadence", "qualcomm", "uber"]
+    sites = ["news.yahoo.com", "finance.yahoo.com"]
+    getLinks(stocks, sites, "2018-01-01", "2018-02-01")
+
+if __name__ == "__main__":
+    main()
